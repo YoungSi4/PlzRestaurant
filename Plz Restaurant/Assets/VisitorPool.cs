283f3517f08e7ecc33ee7d0e16b8f46964fd9c03
YoungSi4 <dksdudgns01@gmail.com>
2025-05-13 21:58:59 +0900

    On Task : visitor spawner

---
+++ Plz Restaurant/Assets/VisitorPool.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorPool : MonoBehaviour
+{
+    public int poolsize;
+    public GameObject prefab;
+
+    // stack È°¿ë
+    public Stack<Visitor> visitorStack = new ();
+
+        void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
