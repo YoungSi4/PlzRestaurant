d7cc4742895061c52054e69ab323ef7ec497660b
Bam2 <1004jangsan@gmail.com>
2025-05-13 21:58:55 +0900

    시작버튼 및 타이머 제작중

---
+++ Plz Restaurant/Assets/TimeControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+    public Time[] Test;
+    // public TextMeshPro[] TimeText;
+
+    void Start()
+    {
+        TimeText[0].text = "08";
+        TimeText[1].text = "00";
+    }
+
+    void Update()
+    {
+        
+    }
+}
