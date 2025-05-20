777e5564a1ec4dc9197169cf34351ddf480b28d0
YoungSi4 <dksdudgns01@gmail.com>
2025-05-20 22:58:33 +0900

    Scriptable Object - FoodData class

---
+++ Plz Restaurant/Assets/Scripts/Food/FoodData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FoodType
+{
+    Korean, Japanese, Chinese, Italian, French, American
+}
+
+[CreateAssetMenu(fileName = "New Item Data", menuName = "CustomData/Create Item Data")]
+public class FoodData : ScriptableObject // 스크립터블 오브젝트 상속
+{
+    public Sprite foodImage;
+    public string foodName;
+
+    [TextArea]
+    public string foodDescription;
+    public FoodType foodType;
+}
