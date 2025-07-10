c3999fbcba5a97df8c0f6a12103b90bcf98d8cc3
Bam2 <1004jangsan@gmail.com>
2025-07-11 03:13:41 +0900

    Function Seperation on FoodManager and TrayControl exactly

--- Plz Restaurant/Assets/Scripts/Food/FoodManager.cs
+++ Plz Restaurant/Assets/Scripts/Food/FoodManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FoodManager : Singleton<FoodManager>
 {
+    [SerializeField] private int foodNum; // 주문이 들어온 음식의 인덱스(임시로 직렬화 사용중)
+    private float cookTime = 1f; // 음식 조리에 걸리는 시간(임시) 
     private Queue<FoodData> completedFood = new Queue<FoodData>();
+
+    private FoodDB foodDB;
+
+    private void Start()
+    {
+        foodDB = FindObjectOfType<FoodDB>();
+    }
     public override void Awake()
     {
         base.Awake();
     }
-
     // 큐에 음식 넣기
     public void AddCompletedFood(FoodData food)
     {
@@ -23,4 +32,38 @@ public class FoodManager : Singleton<FoodManager>
     {
         return HasFood() ? completedFood.Dequeue() : null;
     }
+
+    private void Update()
+    {
+        // 테스트용 큐에 음식 넣기
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int testFoodNum = 1; // 테스트할 음식 인덱스 번호
+            CookFood(testFoodNum);
+            Debug.Log("음식 조리 시작: " + testFoodNum + "번");
+        }
+    }
+    // 음식 조리하기
+    public void CookFood(int foodNum)
+    {
+        StartCoroutine(CookFoodCoroutine(foodNum));
+    }
+    IEnumerator CookFoodCoroutine(int foodNum)
+    {
+        FoodData food = foodDB.GetFoodData(foodNum);
+
+        if (food == null)
+        {
+            Debug.Log("음식 정보 Null 오류. foodNum = " + foodNum);
+            yield break;
+        }
+        
+        // 조리시간만큼 기다리기
+        // 조리시간이 음식마다 다르다면 FoodData에 해당 변수(cookTime) 추가해도 될듯?
+        yield return new WaitForSeconds(cookTime);
+
+        AddCompletedFood(food);
+        Debug.Log("조리 완료 " + foodNum + "번");
+    }
+
 }
--- Plz Restaurant/Assets/Scripts/Food/TrayControl.cs
+++ Plz Restaurant/Assets/Scripts/Food/TrayControl.cs
@@ -10,32 +10,8 @@ public class TrayControl : MonoBehaviour
     // 현재 트레이에 올라가있는 음식 오브젝트 저장
     public GameObject foodObj1;
     public GameObject foodObj2;
-
     void Update()
     {
-        // 테스트용 음식추가 로직
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            FoodData food = Resources.Load<FoodData>("GameObject/Food/Ramen");
-            FoodData food2 = Resources.Load<FoodData>("GameObject/Food/Spaghetti");
-
-            int ran = Random.Range(0, 2);
-            if (food != null && food2 != null)
-            {
-                if (ran == 0)
-                {
-                    FoodManager.Instance.AddCompletedFood(food);
-                    Debug.Log("음식추가1번");
-                }
-                if (ran == 1)
-                {
-                    FoodManager.Instance.AddCompletedFood(food2);
-                    Debug.Log("음식추가2번");
-                }
-            }
-            else Debug.Log("Null오류");
-
-        }
         // 큐에 음식이 있으면서 트레이에 빈자리가 있으면 트레이에 음식을 채우기 위해 계속 확인
         TryUpdateTray();
     }
