2e6a6d6ccfea9704e3c62ef3edb78fb96862df75
tjdwns1314 <tjdwns1314@gmail.com>
2025-07-13 19:22:45 +0900

    1)UI FrontScene에 press any key UI 만듦, audio도 일단 구해서 넣어봄 2) 기현상관련 스크립트 틀을 짜봄 3) headchef관련 스크립트 틀을 짜봄 4) Player 스크립트 대충 만들기만함 5) PlayerInteraction 스크립트 없앰.

--- Plz Restaurant/Assets/Scripts/Player/MoveAndToggle.cs
+++ Plz Restaurant/Assets/Scripts/Player/MoveAndToggle.cs
@@ -19,27 +19,28 @@ public class MoveAndToggle : MonoBehaviour
     public Transform playerBody; //좌우 회전용 (몸통 전체)
     public Transform cameraHolder; // 상하 회전용 (카메라 부모)
 
-    float mouseSensitivity = 10f; //마우스 감도값
-    public float xRotation = 0f; // 카메라 상하 회전 값을 담을 변수
-    public float mouseY;
-
-
+    float mouseSensitivity = 1f; //마우스 감도값
+    float xRotation = 0f; // 카메라 상하 회전 값을 담을 변수
+    float mouseY;
+    float mouseX;
+    float distance = 3f; //레이 거리
+    Ray ray;
     public Transform cameraTransform;
 
     Vector2 moveInput; //인풋시스템에서 키보드로 받을 벡터(x,y값으로 키보드 값을 받기 때문에 vector2)
     Vector3 camForward; // 카메라 앞뒤를 다룰 벡터 
     Vector3 camRight; // 카메라 왼쪽 오른쪽을 다룰 벡터 
-
+    Vector2 mouseInput;
     public Vector3 direction { get; private set; } //플레이어의 이동방향을 담을 벡터
-
+    Vector3 direction3;
     public PlayerInput playerInput;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
-        Cursor.lockState = CursorLockMode.Locked; //마우스를 화면 중앙에 고정
-        Cursor.visible = false; //마우스 커서를 안 보이게 함
+        //Cursor.lockState = CursorLockMode.Locked; //마우스를 화면 중앙에 고정
+        //Cursor.visible = false; //마우스 커서를 안 보이게 함
     }
 
     void Update()
@@ -49,6 +50,7 @@ public class MoveAndToggle : MonoBehaviour
             SwitchToView();
         }
         Move();
+        RaySystem();
     }
     public void OnToggleInput(InputAction.CallbackContext ctx)  //On???Input 형태로 항상 작성해야하는듯
     {
@@ -68,19 +70,38 @@ public class MoveAndToggle : MonoBehaviour
             playerInput.SwitchCurrentActionMap("ThreePerspective");
     }
 
-
+    void RaySystem()
+    {
+        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+        if (isFirstPerson)
+        {
+            if (Physics.Raycast(ray, out RaycastHit hit, distance))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+                if (hit.collider.CompareTag("Visitor"))
+                {
+                    //Debug.Log("Visitor 발견!");
+                    if (Input.GetKeyDown(KeyCode.E)) //이건 인풋시스템으로 고쳐야하는데 1인칭일 때 3인칭일 때 아직 어케 할지 몰라서 쉽게 만들어 놓기만함
+                    {
+                        Debug.Log("visitor상호작용");
+                    }
+                }
+            }
+        }
+    }
 
     //인풋시스템으로 이동 값 받음.
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>(); 
     }
+
     //인풋시스템으로 마우스의 2D 벡터(x:좌우,y:상하)로 받아옴.
     public void OnMouseInput(InputAction.CallbackContext context)
     {
-        Vector2 mouseInput = context.ReadValue<Vector2>(); //2D 벡터(x:좌우,y:상하)로 받아옴.
+        mouseInput = context.ReadValue<Vector2>(); //2D 벡터(x:좌우,y:상하)로 받아옴.
 
-        float mouseX = mouseInput.x * mouseSensitivity * Time.deltaTime; 
+        mouseX = mouseInput.x * mouseSensitivity * Time.deltaTime; 
         mouseY = mouseInput.y* mouseSensitivity * Time.deltaTime;
         //Time.deltaTime으로 일관성 있게 받아주고
 
@@ -104,7 +125,7 @@ public class MoveAndToggle : MonoBehaviour
     {
         if (!isFirstPerson && moveInput != Vector2.zero) //3인칭 이고 입력받는 값이 존재할때(키보드가 눌릴때?)
         {
-            Vector3 direction3 = new Vector3(moveInput.x, 0, moveInput.y); //3인칭일 때는 그냥 인풋값자체를 direction으로 받아야함... 카메라 상관없이 좌표대로 움직여야하기 때문에..
+            direction3 = new Vector3(moveInput.x, 0, moveInput.y); //3인칭일 때는 그냥 인풋값자체를 direction으로 받아야함... 카메라 상관없이 좌표대로 움직여야하기 때문에..
             playerBody.rotation = Quaternion.LookRotation(direction3);
             rb.velocity = direction3.normalized * moveSpeed;
             //플레이어의 rotation을 부드럽게 회전해줌.
@@ -121,7 +142,7 @@ public class MoveAndToggle : MonoBehaviour
             camForward.Normalize(); //벡터의 길이를 1로 만듦(방향만 받아준다?)
             camRight.Normalize();
 
-            Vector3 direction = camRight * moveInput.x + camForward * moveInput.y;
+            direction = camRight * moveInput.x + camForward * moveInput.y;
             // moveInput.x는 양 옆(A/D)으로 이동하는 값을 받는거고 moveInput.y는 앞뒤(W/S)로 이동하는 값을 받는다.
             // 대각선으로 가기 위해서는 둘다 같이 받아서 정의해줘야 힘이 같이 실린다.
 
---
+++ Plz Restaurant/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    string P_name;
+    float P_ownItem;
+    float P_speed;
+
+    void P_refuseOrder()
+    {
+
+    }
+
+    void P_killAnomaly()
+    {
+
+    }
+}
