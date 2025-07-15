8ff23307a4bfa0157bb58cf90eb301296cf1d72e
tjdwns1314 <tjdwns1314@gmail.com>
2025-07-15 13:56:53 +0900

    MainScene1을 만들고 있음 여러 UI들 추가하고 버튼 누르면 창이 열리게끔 까지 만듦

--- Plz Restaurant/Assets/Scripts/UI/MainScene1.cs
+++ Plz Restaurant/Assets/Scripts/UI/MainScene1.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainScene1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //public Button dropDownMenu;
+    public GameObject menu;
+    public GameObject storeMenu;
+    public GameObject ExitMenu;
+    public TextMeshProUGUI asset;
+    public int a = 10000000;
+
+    public void DropDownMenu()
+    {
+        if (menu.activeSelf)
+        {
+            menu.SetActive(false);
+        }
+        else menu.SetActive(true);
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// //////////////////////////////////////////////////
+    public void Asset()
     {
-        
+        asset.text = "asset" + "FormatNumber(a)";
+    }
+    string FormatNumber(int num)
+    {
+        if (num >= 1_000_000_000)
+        {
+            return (num / 1_000_000_000f).ToString("0.##") + "B"; //정수 부분은 무조건 출력 소수점은 2자리만 출력
+        }
+        else return num.ToString();
+    }
+    //////////////////////////////////////////////////////
+    
+    public void OnStore()
+    {
+        if (storeMenu.activeSelf)
+        {
+            storeMenu.SetActive(false);
+        }
+        else storeMenu.SetActive(true);
+    }
+
+    public void OnExitMenu()
+    {
+        if (ExitMenu.activeSelf)
+        {
+            ExitMenu.SetActive(false);
+        }
+        else ExitMenu.SetActive(true);
+    }
+    public void GameStart()
+    {
+        Debug.Log("시작");
     }
 }
